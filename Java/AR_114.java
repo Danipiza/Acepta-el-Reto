06a84169f0a0b70eedad692ebbc72908da44c2fd
DannyP39 <dpizarrogallego@gmail.com>
2024-10-10 09:48:45 +0200

    Added Java exercises [114, 116, 126]

---
+++ Java/AR_114.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+	
+    public static void resuelveCaso() {
+        System.out.println("Hola mundo.");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+		
+        int n=scanner.nextInt();
+        for (int i=0;i<n;i++) resuelveCaso();
+		
+        scanner.close();
+    }
+}
